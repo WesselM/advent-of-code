9d425e15a3137a642b8cf699a54fd0756b8eadd3
WesselM <40270039+WesselM@users.noreply.github.com>
2024-07-07 19:06:23 +0200

    Add solutions a and b for day 1 of 2015

--- 2015/day_01.py
+++ 2015/day_01.py
@@ -2,24 +2,41 @@
 
 def main():
     with open("2015/examples/ex_01.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(str, f.read().strip()))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == -1
+    assert part_two(ex) == 5
 
-    # with open("2015/input/inp_01.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2015/input/inp_01.txt", "r") as f:
+        inp = list(map(str, f.read().strip()))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(instructions):
+    position = 0
+    for instruction in instructions:
+        if instruction == "(":
+            position += 1
+        elif instruction == ")":
+            position -= 1
+
+    return position
+
+
+def part_two(instructions):
+    position = 0
+    for i, instruction in enumerate(instructions):
+        if instruction == "(":
+            position += 1
+        elif instruction == ")":
+            position -= 1
+
+        if position < 0:
+            break
+
+    return i + 1
 
 
 if __name__ == '__main__':
