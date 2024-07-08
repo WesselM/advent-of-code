5a3f032d2303a82d6a254da089cc773df2859876
WesselM <40270039+WesselM@users.noreply.github.com>
2024-07-08 18:45:54 +0200

    Add solution for part b for day 3 of 2016

--- 2016/day_03.py
+++ 2016/day_03.py
@@ -7,14 +7,14 @@ def main():
     with open("2016/examples/ex_03.txt", "r") as f:
         ex = list(map(str, f.read().strip().split("\n")))
 
-    assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 3
+    assert part_two(ex) == 6
 
     with open("2016/input/inp_03.txt", "r") as f:
         inp = list(map(str, f.read().strip().split("\n")))
 
     print("Part one:", part_one(inp))
-    # print("Part two:", part_two(ex))
+    print("Part two:", part_two(inp))
 
 
 def part_one(triangles: list[str]):
@@ -26,8 +26,17 @@ def part_one(triangles: list[str]):
         triangles)))
 
 
-def part_two(triangles):
-    return 0
+def part_two(triangles: list[str]):
+    triangles = list(map(lambda triangle:
+                         list(map(int, triangle.split())), triangles))
+
+    triangles = [combination for combinations in [list(zip(*triangles[i:i+3]))
+                 for i in range(0, len(triangles) - 2, 3)]
+                 for combination in combinations]
+
+    return sum(list(starmap(
+        lambda a, b, c: a + b > c and a + c > b and b + c > a,
+        triangles)))
 
 
 if __name__ == '__main__':
