d125886c1ea4c3a03f12084034bcb99d23979e3a
WesselM <40270039+WesselM@users.noreply.github.com>
2024-07-08 18:23:36 +0200

    Add complex solution for part a for day 3 of 2016

--- 2016/day_03.py
+++ 2016/day_03.py
@@ -1,26 +1,37 @@
 # https://adventofcode.com/2016/day/3
 
+from itertools import starmap
+
+
 def main():
     with open("2016/examples/ex_03.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(str, f.read().strip().split("\n")))
 
-    # assert part_one(ex) == 1
+    # assert part_one(ex) == 3
     # assert part_two(ex) == 1
 
-    # with open("2016/input/inp_03.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2016/input/inp_03.txt", "r") as f:
+        inp = list(map(str, f.read().strip().split("\n")))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    # print("Part two:", part_two(ex))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(triangles: list[str]):
+    triangles = list(map(lambda triangle:
+                         tuple(map(int, triangle.split())), triangles))
 
+    triangles = list(starmap(
+        lambda a, b, c: [((a, b), c), ((a, c), b), ((b, c), a)],
+        triangles))
+
+    return sum([all(starmap(lambda two_side, remainder:
+                            sum(two_side) > remainder,
+                            triangle)) for triangle in triangles])
+
+
+def part_two(triangles):
+    return 0
 
 if __name__ == '__main__':
     main()
