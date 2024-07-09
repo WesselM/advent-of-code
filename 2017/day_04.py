c78851d1bb87351234b844133168708889a10cff
WesselM <40270039+WesselM@users.noreply.github.com>
2024-07-10 00:35:41 +0200

    Add solutions a and b for days 4 and 5 of 2017

--- 2017/day_04.py
+++ 2017/day_04.py
@@ -2,24 +2,31 @@
 
 def main():
     with open("2017/examples/ex_04.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(str, f.read().strip().split("\n")))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 7
+    assert part_two(ex) == 5
 
-    # with open("2017/input/inp_04.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2017/input/inp_04.txt", "r") as f:
+        inp = list(map(str, f.read().strip().split("\n")))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(passphrases: list[str]):
+    passphrases = list(map(lambda passphrase: passphrase.split(), passphrases))
+    return sum([len(passphrase) == len(set(passphrase))
+               for passphrase in passphrases])
+
+
+def part_two(passphrases):
+    passphrases = list(map(lambda passphrase: ["".join(sorted(word))
+                                               for word in passphrase.split()],
+                           passphrases))
+
+    return sum([len(passphrase) == len(set(passphrase))
+               for passphrase in passphrases])
 
 
 if __name__ == '__main__':
--- 2017/day_05.py
+++ 2017/day_05.py
@@ -2,24 +2,39 @@
 
 def main():
     with open("2017/examples/ex_05.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(int, f.read().strip().split()))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 5
+    assert part_two(ex) == 10
 
-    # with open("2017/input/inp_05.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2017/input/inp_05.txt", "r") as f:
+        inp = list(map(int, f.read().strip().split()))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(instructions: list[int]):
+    index = counter = 0
+    instructions = instructions.copy()
+    while index < len(instructions):
+        offset = index + instructions[index]
+        instructions[index] += 1
+        index = offset
+        counter += 1
+
+    return counter
+
+
+def part_two(instructions: list[int]):
+    index = counter = 0
+    while index < len(instructions):
+        offset = index + instructions[index]
+        instructions[index] += -1 if instructions[index] >= 3 else 1
+        index = offset
+        counter += 1
+
+    return counter
 
 
 if __name__ == '__main__':
