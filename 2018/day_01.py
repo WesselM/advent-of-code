a00e117a19dd04df20e835b4455c45d6ba6c1fc9
WesselM <40270039+WesselM@users.noreply.github.com>
2024-07-16 18:48:08 +0200

    Add solutions a and b for days 1 and 2 of 2018

--- 2018/day_01.py
+++ 2018/day_01.py
@@ -2,24 +2,32 @@
 
 def main():
     with open("2018/examples/ex_01.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(int, f.read().strip().split()))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 3
+    assert part_two(ex) == 2
 
-    # with open("2018/input/inp_01.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2018/input/inp_01.txt", "r") as f:
+        inp = list(map(int, f.read().strip().split()))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(frequency_changes):
+    return sum(frequency_changes)
+
+
+def part_two(frequency_changes):
+    index = current = 0
+    reached = set()
+    while (True):
+        current += frequency_changes[index]
+        index = (index + 1) % len(frequency_changes)
+        if current in reached:
+            return current
+        else:
+            reached.add(current)
 
 
 if __name__ == '__main__':
--- 2018/day_02.py
+++ 2018/day_02.py
@@ -1,25 +1,38 @@
 # https://adventofcode.com/2018/day/2
 
+from functools import reduce
+from operator import mul
+from string import ascii_lowercase
+
+
 def main():
     with open("2018/examples/ex_02.txt", "r") as f:
         ex = list(map(str, f.read().strip().split()))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 12
+    assert part_two(ex) == "abcde"
 
-    # with open("2018/input/inp_02.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2018/input/inp_02.txt", "r") as f:
+        inp = list(map(str, f.read().strip().split()))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(ids: list[str]):
+    return reduce(mul, [sum(counts) for counts in zip(*[
+        (any([id.count(letter) == 2 for letter in ascii_lowercase]),
+         any([id.count(letter) == 3 for letter in ascii_lowercase]))
+        for id in ids
+    ])])
+
+
+def part_two(ids: list[str]):
+    for id1 in ids:
+        for id2 in ids:
+            overlap = "".join([a for a, b in zip(id1, id2) if a == b])
+            if len(overlap) == len(id1) - 1:
+                return overlap
 
 
 if __name__ == '__main__':
