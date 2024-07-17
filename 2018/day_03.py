d7259264d34ac9361de27f1861ce117af860580e
WesselM <40270039+WesselM@users.noreply.github.com>
2024-07-18 00:39:11 +0200

    Add solutions a and b for day 3 of 2018

--- 2018/day_03.py
+++ 2018/day_03.py
@@ -1,25 +1,48 @@
 # https://adventofcode.com/2018/day/3
 
+import re
+
+
 def main():
     with open("2018/examples/ex_03.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(str, f.read().strip().split("\n")))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 4
+    assert part_two(ex) == 3
 
-    # with open("2018/input/inp_03.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2018/input/inp_03.txt", "r") as f:
+        inp = list(map(str, f.read().strip().split("\n")))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(claims: list[str]):
+    fabric = [[set() for _ in range(1000)] for _ in range(1000)]
+    for claim in claims:
+        id, d_left, d_top, width, height = \
+            map(int,
+                re.match(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', claim).groups())
+        for y in range(d_top, d_top + height):
+            for x in range(d_left, d_left + width):
+                fabric[y][x].add(id)
+
+    return sum(len(ids) >= 2 for row in fabric for ids in row)
+
+
+def part_two(claims: list[str]):
+    fabric = [[set() for _ in range(1000)] for _ in range(1000)]
+    for claim in claims:
+        id, d_left, d_top, width, height = \
+            map(int,
+                re.match(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', claim).groups())
+        for y in range(d_top, d_top + height):
+            for x in range(d_left, d_left + width):
+                fabric[y][x].add(id)
+
+    for i in range(1, id + 1):
+        if all(len(ids) == 1 for row in fabric for ids in row if i in ids):
+            return i
 
 
 if __name__ == '__main__':
