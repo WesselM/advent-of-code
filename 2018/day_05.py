786ac9f5f166a81943413256634eb524df0eaf24
WesselM <40270039+WesselM@users.noreply.github.com>
2024-08-26 15:19:52 +0200

    Add solutions a and b for day 5 of 2018

--- 2018/day_05.py
+++ 2018/day_05.py
@@ -2,24 +2,35 @@
 
 def main():
     with open("2018/examples/ex_05.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = f.read().strip()
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 10
+    assert part_two(ex) == 4
 
-    # with open("2018/input/inp_05.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2018/input/inp_05.txt", "r") as f:
+        inp = f.read().strip()
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def react(polymer):
+    remaining = []
+    for unit in polymer:
+        if remaining and unit == remaining[-1].swapcase():
+            remaining.pop()
+        else:
+            remaining.append(unit)
+    return remaining
+
+
+def part_one(polymer: str):
+    return len(react(polymer))
+
+
+def part_two(polymer: str):
+    return min(len(react(polymer.replace(c, "").replace(c.upper(), "")))
+               for c in set(polymer.lower()))
 
 
 if __name__ == '__main__':
