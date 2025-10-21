0c491b87a03cd40d75ee17615face884fd42fb6a
WesselM <40270039+WesselM@users.noreply.github.com>
2025-10-22 00:28:27 +0200

    Add solutions a and b for day 2 of 2022

--- 2022/day_02.py
+++ 2022/day_02.py
@@ -1,25 +1,34 @@
 # https://adventofcode.com/2022/day/2
 
+
 def main():
     with open("2022/examples/ex_02.txt", "r") as f:
-        ex = list(map(str, f.read().strip().split()))
+        ex = list(map(str, f.read().strip().split("\n")))
 
-    # assert part_one(ex) == 1
-    # assert part_two(ex) == 1
+    assert part_one(ex) == 15
+    assert part_two(ex) == 12
 
-    # with open("2022/input/inp_02.txt", "r") as f:
-    #     inp = list(map(str, f.read().strip().split()))
+    with open("2022/input/inp_02.txt", "r") as f:
+        inp = list(map(str, f.read().strip().split("\n")))
 
-    print("Part one:", part_one(ex))
-    # print("Part two:", part_two(inp))
-
-
-def part_one(entries):
-    return entries[0]
+    print("Part one:", part_one(inp))
+    print("Part two:", part_two(inp))
 
 
-def part_two(entries):
-    return entries[0]
+def part_one(strategy_guide):
+    YOU = {'X': 1, 'Y': 2, 'Z': 3}
+    OPP = {'A': 1, 'B': 2, 'C': 3}
+    RES = {0: 3, 1: 6, 2: 0}
+    return sum(RES[(YOU[y]-OPP[o]) % 3] + YOU[y]
+               for o, y in (line.split() for line in strategy_guide))
+
+
+def part_two(strategy_guide):
+    YOU = {'X': 0, 'Y': 3, 'Z': 6}
+    OPP = {'A': 1, 'B': 2, 'C': 3}
+    MOVE = {0: 3, 1: 1, 2: 2}
+    return sum(YOU[y] + MOVE[(OPP[o] + (YOU[y] // 3 - 1)) % 3]
+               for o, y in (line.split() for line in strategy_guide))
 
 
 if __name__ == '__main__':
